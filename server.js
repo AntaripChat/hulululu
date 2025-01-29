42f3181f909a0f13113a7a5d04ad3aeeaaac4dbf
Antarip Chatterjee <antaripchatterjee@gmail.com>
2025-01-29 10:52:37 +0530

    first commit

--- server.js
+++ server.js
@@ -20,31 +20,47 @@ const chatbotResponse = (message) => {
     const greetings = ['hi', 'hello', 'hey', 'howdy'];
     const byeMessage = ['bye', 'goodbye', 'see you', 'later'];
     const okMessage = ['ok', 'okay', 'sure', 'fine', 'good', 'great', 'awesome', 'cool', 'nice', 'super', 'fantastic', 'excellent', 'wonderful', 'amazing', 'brilliant', 'perfect', 'fabulous', 'terrific']
+    const loveYou = ['I love you', 'love you', 'love u', 'luv u', 'luv you', 'i luv you', 'i luv u', 'i love u', 'i love you', 'i like you', 'like you', 'like u']
+    const howAreyou = ['how are you', 'how r u', 'how are u', 'how r you', 'how are you?', 'how r u?', 'how are u?', 'how r you?']
+    const missYou = ['miss you', 'miss u', 'missing you', 'missing u', 'i miss you', 'i miss u', 'i am missing you', 'i am missing u']
+    const love = ['❤️❤️','❤️','💖','💖💖','💖💖💖','💖💖💖💖','💖💖💖💖💖','💖💖💖💖💖💖','💖💖💖💖💖💖💖','💖💖💖💖💖💖💖💖','💖💖💖💖💖💖💖💖💖']
+
     const lowercaseMessage = message.toLowerCase();
 
     if (greetings.some((greeting) => lowercaseMessage.includes(greeting))) {
-        return 'Hello! I am Antarip Chat Bot';
+        return 'Hello! BABY! 😍';
     }
     if(byeMessage.some((bye) => lowercaseMessage.includes(bye))){
-        return 'Goodbye! Have a great day!';
+        return 'Goodbye baby! Have a great day! 😘';
     }
     if(okMessage.some((ok) => lowercaseMessage.includes(ok))){
         return 'Great!';
     }
     if(goodMorning.some((gm) => lowercaseMessage.includes(gm))){
-        return 'Good Morning!';
+        return 'Good Morning!❤️';
     }
     if(goodAfternoon.some((ga) => lowercaseMessage.includes(ga))){
-        return 'Good Afternoon!';
+        return 'Good Afternoon!❤️';
     }
     if(goodEvening.some((ge) => lowercaseMessage.includes(ge))){
-        return 'Good Evening!';
+        return 'Good Evening!❤️';
     }
     if(goodNight.some((gn) => lowercaseMessage.includes(gn))){
         return 'Good Night!';
     }
-
-    return 'Sorry Bro I am not a Google, Are Padhai Karlo Thoda.';
+    if(loveYou.some((love) => lowercaseMessage.includes(love))){
+        return 'I love you too! 💖 😘';
+    }
+    if(howAreyou.some((how) => lowercaseMessage.includes(how))){
+        return 'I am good, Thank you! How are you?';
+    }
+    if(missYou.some((miss) => lowercaseMessage.includes(miss))){
+        return 'I miss you too! 😔';
+    }
+    if(love.some((love) => lowercaseMessage.includes(love))){
+        return ' 💖 😘';
+    }
+    return 'Sorry Baby, I did not understand that! 😕';
 };
 
 app.post('/chat', (req, res) => {
